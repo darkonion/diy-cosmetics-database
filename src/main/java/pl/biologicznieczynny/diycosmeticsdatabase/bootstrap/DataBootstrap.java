962a54dccf8537f0d0422d7c5e1b4dedd1b0159d
darkonion <pawkapl89@gmail.com>
2020-05-29 21:44:44 +0200

    bootstrap development

--- src/main/java/pl/biologicznieczynny/diycosmeticsdatabase/bootstrap/DataBootstrap.java
+++ src/main/java/pl/biologicznieczynny/diycosmeticsdatabase/bootstrap/DataBootstrap.java
@@ -13,11 +13,14 @@ import pl.biologicznieczynny.diycosmeticsdatabase.models.Tool;
 import pl.biologicznieczynny.diycosmeticsdatabase.repositories.IngredientRepository;
 import pl.biologicznieczynny.diycosmeticsdatabase.repositories.RecipeCategoryRepository;
 import pl.biologicznieczynny.diycosmeticsdatabase.repositories.RecipeRepository;
+import pl.biologicznieczynny.diycosmeticsdatabase.repositories.ToolRepository;
 import pl.biologicznieczynny.diycosmeticsdatabase.repositories.UnitOfMeasureRepository;
 
 import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.Set;
+
+import static pl.biologicznieczynny.diycosmeticsdatabase.models.Difficulty.EASY;
 
 @Component
 @Slf4j
@@ -27,15 +30,18 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
     private final RecipeRepository recipeRepository;
     private final RecipeCategoryRepository recipeCategoryRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final ToolRepository toolRepository;
 
     public DataBootstrap(IngredientRepository ingredientRepository,
             RecipeRepository recipeRepository,
             RecipeCategoryRepository recipeCategoryRepository,
-            UnitOfMeasureRepository unitOfMeasureRepository) {
+            UnitOfMeasureRepository unitOfMeasureRepository,
+            ToolRepository toolRepository) {
         this.ingredientRepository = ingredientRepository;
         this.recipeRepository = recipeRepository;
         this.recipeCategoryRepository = recipeCategoryRepository;
         this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.toolRepository = toolRepository;
     }
 
 
@@ -63,12 +69,12 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
         Ingredient etoh = new Ingredient();
         etoh.setName("EtOH");
         etoh.setDescription("W temperaturze pokojowej jest bezbarwną, łatwopalną cieczą o swoistym zapachu i piekącym smaku. W obecności powietrza pali się ona słabo widocznym, niebieskawym płomieniem.");
-        etoh.setBlogUrl("Http://biologicznieczynny.com/etoh");
+        etoh.setImageUrl("https://thumbs.dreamstime.com/b/etanol-w-butelki-ikonie-mieszkanie-styl-79030851.jpg");
 
         Ingredient sheaButter = new Ingredient();
         sheaButter.setName("Masło Shea");
         sheaButter.setDescription("Olej roślinny uzyskiwany z owoców masłosza Parka. W postaci świeżej, nieprzetworzonej ma konsystencję pasty, barwę białawą, jest niemal bezwonny i ma bardzo słabo wyczuwalny smak.cznym, niebieskawym płomieniem.");
-        sheaButter.setBlogUrl("Http://biologicznieczynny.com/etoh");
+        sheaButter.setImageUrl("https://www.gracefruit.com/uploads/images/products/large/gracefruit_gracefruit_organicunrefinedsheabutter_1460545985New_pic_1.jpg");
 
         Ingredient glycerin = new Ingredient();
         glycerin.setName("Gliceryna");
@@ -78,7 +84,7 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
         Ingredient propyleneGlycol = new Ingredient();
         propyleneGlycol.setName("Glikol Propylenowy");
         propyleneGlycol.setDescription("Olej roślinny uzyskiwany z owoców masłosza Parka. W postaci świeżej, nieprzetworzonej ma konsystencję pasty, barwę białawą, jest niemal bezwonny i ma bardzo słabo wyczuwalny smak.cznym, niebieskawym płomieniem.");
-        propyleneGlycol.setBlogUrl("Http://biologicznieczynny.com/etoh");
+        propyleneGlycol.setImageUrl("https://procold.pl/wp-content/uploads/2019/04/Depositphotos_6800377_ds-e1554878919448.jpg");
 
 
         ingredientRepository.save(water);
@@ -98,15 +104,42 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
         oilQuantity.setIngredient(linenOil);
         oilQuantity.setUnitOfMeasure(unitOfMeasureRepository.findByUom("ml").get());
 
+        IngredientQuantity linenOilQuantity = new IngredientQuantity();
+        linenOilQuantity.setAmount(5);
+        linenOilQuantity.setIngredient(linenOil);
+        linenOilQuantity.setUnitOfMeasure(unitOfMeasureRepository.findByUom("ml").get());
+
         IngredientQuantity glycerinQuantity = new IngredientQuantity();
         glycerinQuantity.setAmount(3);
         glycerinQuantity.setIngredient(glycerin);
         glycerinQuantity.setUnitOfMeasure(unitOfMeasureRepository.findByUom("Łyżki stołowe").get());
 
+        IngredientQuantity sheaQuantity = new IngredientQuantity();
+        sheaQuantity.setAmount(25);
+        sheaQuantity.setIngredient(sheaButter);
+        sheaQuantity.setUnitOfMeasure(unitOfMeasureRepository.findByUom("g").get());
+
+        IngredientQuantity glycolQuantity = new IngredientQuantity();
+        glycolQuantity.setAmount(50);
+        glycolQuantity.setIngredient(propyleneGlycol);
+        glycolQuantity.setUnitOfMeasure(unitOfMeasureRepository.findByUom("ml").get());
+
+        IngredientQuantity etohQuantity = new IngredientQuantity();
+        etohQuantity.setAmount(50);
+        etohQuantity.setIngredient(etoh);
+        etohQuantity.setUnitOfMeasure(unitOfMeasureRepository.findByUom("ml").get());
+
         Set<IngredientQuantity> ingredientQuantities = new HashSet<>();
         ingredientQuantities.add(oilQuantity);
         ingredientQuantities.add(waterQuantity);
         ingredientQuantities.add(glycerinQuantity);
+
+        Set<IngredientQuantity> ingredientQuantities2 = new HashSet<>();
+
+        ingredientQuantities2.add(sheaQuantity);
+        ingredientQuantities2.add(glycolQuantity);
+        ingredientQuantities2.add(etohQuantity);
+        ingredientQuantities2.add(linenOilQuantity);
 
         Tool mixer = new Tool();
         mixer.setName("Mikser");
@@ -125,13 +158,28 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
         cylinder.setDescription("Cylinder miarowy o pojemności 10ml, im mniejszy tym lepszy");
 
         Step first = new Step();
-        first.setName("First Step Here");
-        first.setDetail("Just a first step, mix it and thats it");
+        first.setName("Krok pierwszy - przygotowanie składników");
+        first.setDetail("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
         Step second = new Step();
-        second.setName("Second Step Here");
-        second.setDetail("Just a second step, mix it and thats it");
+        second.setName("Krok drugi - wymieszanie składników w naczyniu");
+        second.setDetail("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
+        Step third = new Step();
+        third.setName("Krok trzeci - podgrzewanie roztworu");
+        third.setDetail("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+        Step first2 = new Step();
+        first2.setName("Krok pierwszy - przygotowanie składników i pomiar");
+        first2.setDetail("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+        Step second2 = new Step();
+        second2.setName("Krok drugi - wymieszanie składników w naczyniu");
+        second2.setDetail("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+        Step third2 = new Step();
+        third2.setName("Krok trzeci - podgrzewanie roztworu");
+        third2.setDetail("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
 
         Recipe bodyCream = new Recipe();
@@ -147,11 +195,25 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
         Recipe faceCream = new Recipe();
         faceCream.setPrepTime(300);
         faceCream.setName("Malinowy krem do Twarzy");
-        faceCream.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        faceCream.setDescription("Istnieje wiele legend, bardziej miejskich, niż ludowych, które narosły wokół robienia kosmetyków w domu. Jedną z nich jest właśnie konieczność znajomości chemii. Nie musisz być ekspertką w rozrysowywaniu wiązań chemicznych, żeby ukręcić samodzielnie krem. Wystarczy, że poświęcisz trochę czasu na poczytanie o właściwościach poszczególnych składników. To trochę tak jak z wiedzą o odżywianiu - możesz zdrowo jeść przestrzegając z grubsza proporcji zawartych w piramidzie żywienia, świadomość wszystkich złożonych procesów wywoływanych w organizmie przez chleb pszenny i pomidory nie jest niezbędna.\n" +
+                "Używanie kremów ekologicznych nie jest poświęceniem i przestawieniem się na gorsze produkty. Ekologia stanowi raczej miły dodatek - pięknieje twoja skóra i pięknieje świat. Podstawowa przewaga takich kremów nad tymi drogeryjnymi jest taka, że doskonale wiemy, co jest w naszym słoiczku. Firmy kosmetyczne kuszą nas koenzymem Q10, a reklamowany w ten sposób kosmetyk może zawierać śladowe ilości substancji, niewyczuwalne dla skóry. Składy na opakowaniach są podawane wedle zasady \"od największej do najmniejszej zawartości\", wyszczególnienia procentowe należą do rzadkości. Zakraplając witaminę E i olej arganowy do samodzielnie zrobionego kremu widzimy za co płacimy. Co więcej, składy drogeryjnych specyfików są standaryzowane.");
         faceCream.setDifficulty(Difficulty.HARD);
         faceCream.setImageUrl("http://biologicznieczynny.pl/wp-content/uploads/2019/09/2-1.jpg");
         faceCream.setCategories(Set.of(recipeCategoryRepository.findById(1L).get(), recipeCategoryRepository.findById(3L).get()));
         faceCream.setIntro("Zaawansowany krem do twarzy na noc, pozytywne efekty już po 3 dniach stosowania! ");
+
+        linenOilQuantity.setRecipe(faceCream);
+        sheaQuantity.setRecipe(faceCream);
+        etohQuantity.setRecipe(faceCream);
+        glycolQuantity.setRecipe(faceCream);
+        faceCream.setIngredientQuantities(ingredientQuantities2);
+
+        faceCream.setTools(Set.of(cylinder, mixer, jewelryWeight));
+
+        first2.setRecipe(faceCream);
+        second2.setRecipe(faceCream);
+        third2.setRecipe(faceCream);
+        faceCream.setSteps(Set.of(first2, second2, third2));
         recipeRepository.save(faceCream);
 
         Recipe headShampoo = new Recipe();
@@ -184,17 +246,27 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
         faceSerum.setIntro("Serum do twarzy, nie wiem co robi, bo nie mam pojęcia czym jest serum");
         recipeRepository.save(faceSerum);
 
+        Recipe perfumeOil = new Recipe();
+        perfumeOil.setPrepTime(100);
+        perfumeOil.setName("Olejek zapachowy");
+        perfumeOil.setDescription("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        perfumeOil.setDifficulty(Difficulty.EASY);
+        perfumeOil.setImageUrl("http://biologicznieczynny.pl/wp-content/uploads/2019/09/3.jpg");
+        perfumeOil.setCategories(Set.of(recipeCategoryRepository.findById(3L).get()));
+        perfumeOil.setIntro("Olejek zapachowy uwalniający zapach pod wpływem ciepła, idealny do relaksu w domowym SPA");
+        recipeRepository.save(perfumeOil);
+
         Recipe oilBalm = new Recipe();
         oilBalm.setPrepTime(100);
         oilBalm.setName("Olejek do ciała");
         oilBalm.setDescription("W potocznym i marketingowym języku słowo \"olejek\" używane jest niewłaściwie do określenia kosmetyków składających się z rozpuszczalnych w tłuszczach (lipofilowych) składników. W kosmetologii nazwa olejki zarezerwowana jest dla olejków eterycznych (lotnych destylatów z roślin). Z kolei kosmetyki składające się z lipidowych składników to po prostu oliwki. Zrobienie oliwki zajmie Ci zaledwie kilka minut. Ważne, aby poznać kilka zasad łączenia składników, poza tym będziesz mogła tworzyć własne kosmetyki według preferencji i pomysłowości oraz jakości porównywalnej do najlepszych kosmetyków na rynku.");
-        oilBalm.setDifficulty(Difficulty.EASY);
+        oilBalm.setDifficulty(EASY);
         oilBalm.setImageUrl("http://biologicznieczynny.pl/wp-content/uploads/2020/02/6.jpg");
         oilBalm.setCategories(Set.of(recipeCategoryRepository.findById(1L).get(), recipeCategoryRepository.findById(3L).get()));
-        oilBalm.setIntro("Olejek do ciała dla leniwych, tylko trzy składniki! :) ");
+        oilBalm.setIntro("Aromatyczny olejek do ciała dla leniwych, tylko trzy składniki!");
 
 
-        oilBalm.setTools(Set.of(spoon, mixer));
+        oilBalm.setTools(Set.of(spoon));
 
         waterQuantity.setRecipe(oilBalm);
         oilQuantity.setRecipe(oilBalm);
@@ -203,7 +275,8 @@ public class DataBootstrap implements ApplicationListener<ContextRefreshedEvent>
 
         first.setRecipe(oilBalm);
         second.setRecipe(oilBalm);
-        oilBalm.setSteps(Set.of(first, second));
+        third.setRecipe(oilBalm);
+        oilBalm.setSteps(Set.of(first, second, third));
 
         recipeRepository.save(oilBalm);
     }
--- src/main/java/pl/biologicznieczynny/diycosmeticsdatabase/repositories/ToolRepository.java
+++ src/main/java/pl/biologicznieczynny/diycosmeticsdatabase/repositories/ToolRepository.java
@@ -3,11 +3,11 @@ package pl.biologicznieczynny.diycosmeticsdatabase.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.biologicznieczynny.diycosmeticsdatabase.models.Tool;
 
-@Repository
+@RepositoryRestResource(exported = false)
 public interface ToolRepository extends JpaRepository<Tool, Long> {
 
     Page<Tool> findByRecipesId(@RequestParam("id") Long id, Pageable pageable);
